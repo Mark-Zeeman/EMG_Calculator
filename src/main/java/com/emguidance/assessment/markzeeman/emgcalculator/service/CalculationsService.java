4523547baa2dc10926626a70dcc28dbfc1e1f467
Mark Zeeman <mark@emguidance.com>
2020-09-15 22:42:11 +0200

    No comments

--- src/main/java/com/emguidance/assessment/markzeeman/emgcalculator/service/CalculationsService.java
+++ src/main/java/com/emguidance/assessment/markzeeman/emgcalculator/service/CalculationsService.java
@@ -37,8 +37,6 @@ public class CalculationsService {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getCalculation(@PathParam("id") Long id) {
         Calculation calculation = repository.findById(id).orElseThrow(NotFoundException::new);
-        // I am an edited comment
-        // I am an edited comment
 
         logger.info("Processing getCalculation{id}" + calculation);
         return Response.ok(calculation).build();
