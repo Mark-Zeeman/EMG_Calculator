2cdfe1fead4e2f5a8172696586c9f9371566f202
Mark Zeeman <mark@emguidance.com>
2020-09-22 16:38:22 +0200

    Updating the master

--- src/main/java/com/emguidance/assessment/markzeeman/emgcalculator/service/CalculationsService.java
+++ src/main/java/com/emguidance/assessment/markzeeman/emgcalculator/service/CalculationsService.java
@@ -51,6 +51,9 @@ public class CalculationsService {
 
         logger.info("Processing postCalculation:  inputNumber1:" + inputNumber1+ "     inputNumber2"+ inputNumber2 + "    operator:"+operator );
 
+        logger.info("Processing New Log:  inputNumber1:" + inputNumber1+ "     inputNumber2"+ inputNumber2 + "    operator:"+operator );
+
+
         String response =null;
         // Mark
         String b=null;
